84f7785001cf677d0a2050ef4e4da059fe73303a
htntang <htntang@gmail.com>
2023-04-11 17:04:23 -0600

    designed app

---
+++ client/src/Header.js
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <header>
+            <a href="/" className="logo">Inclusive Playworks™</a>
+            <nav>
+                <button class="navigation">Login</button>
+                <button class="navigation">Register</button>
+            </nav>
+        </header>
+    );
+}
