718deb221007eba73545f19dcaeb508b0ae32d66
htntang <htntang@gmail.com>
2023-04-12 12:00:35 -0600

    routed with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import Header from './Header.js';
 import Post from './Post.js';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return(
+    <Routes>
+      <Route index element={
     <main>
       <Header />
       <Post />
 
 
     </main>
-  )
+      } />
+    </Routes>
+  );
 }
 
 export default App;
