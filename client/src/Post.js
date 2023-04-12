8fc5f90d205fb3651109843905c350c083238a5b
htntang <htntang@gmail.com>
2023-04-12 11:13:42 -0600

    prettified the app

---
+++ client/src/Post.js
@@ -0,0 +1,20 @@
+export default function Post(){
+    return(
+        <body>
+        <div className="row">
+        <div className="column">
+          <img src="https://i.imgur.com/c0qpCEI.png" alt="pictures of complex needs children playing"></img>
+        </div>
+        <div className="column">
+          <img src="https://i.imgur.com/ZYScK1W.png" alt="brand logo"></img>
+          
+        </div>
+        </div>
+  
+        <div className="search-button">
+        <button class="search">Find Playground</button>
+        <button class="search">Playground Map</button>
+        </div>
+      </body>
+    );
+}
