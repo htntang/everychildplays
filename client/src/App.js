156b1039cff3c99dad3a2a471f276e2e4181c5f8
htntang <htntang@gmail.com>
2023-04-13 20:03:41 -0600

    added navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,25 @@ import Layout from './Layout';
 import IndexPage from './Pages/IndexPage';
 import LoginPage from './Pages/LoginPage';
 import RegistrationPage from './Pages/RegistrationPage';
+import Navbar from './Pages/navbar';
+import About from './Pages/About';
+import Team from './Pages/Team';
 
 function App() {
   return(
+    <>
+    <Navbar />
+    <div className="container"></div>
     <Routes>
        <Route path="/" element={ <Layout /> }>
         <Route index element = { <IndexPage /> }/>
         <Route path="/login" element={ <LoginPage /> } />
         <Route path="/register" element={ <RegistrationPage /> } />
+        <Route path="/about" element={ <About />} />
+        <Route path="/team" element={ <Team /> } />
       </Route>
     </Routes>
+    </>
   );
 }
 
---
+++ client/src/Pages/navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return<nav className="nav">
+        <ul>
+            <a href="/">Search Playgrounds</a><br></br>
+            <a href="/">Playground Map</a><br></br>
+            <a href="/">Resources</a><br></br>
+            <a href="/about">About Us</a><br></br>
+            <a href="/team">Meet the Team</a>
+        </ul>
+    </nav>
+}
