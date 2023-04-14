10d2da3a694cb6c7e474f4598f9710f1a71c8947
htntang <htntang@gmail.com>
2023-04-13 21:20:49 -0600

    edited the meet the team page

--- client/src/Pages/Team.js
+++ client/src/Pages/Team.js
@@ -1,5 +1,30 @@
 export default function Team() {
     return(
+        <>
         <h1>Meet the Team</h1>
+        <div className="team">
+        <div className="row">
+            <div className="column">
+        <h2>Hikmah Adepoju</h2>
+        <body>"Insert Bio Here"<br></br><br></br><a href="https://www.linkedin.com/in/hikmaha/">Connect with Hikmah on LinkedIn!</a>
+        </body></div>
+        <div className="column">
+        <h2>Oladimeji Falusi</h2>
+        <body>"Insert Bio Here"<br></br><br></br><a href="https://www.linkedin.com/in/oladimeji-falusi-0251a225a/">Connect with Oladimeji on LinkedIn!</a>
+        </body></div>
+        <div className="column">
+        <h2>Han<br></br>Tang</h2>
+        <body>"Insert Bio Here"<br></br><br></br>
+        <a href="https://www.linkedin.com/in/han-tang-49ba799b/">Connect with Han on LinkedIn!</a>
+        </body>
+        </div>
+        <div className="column">
+        <h2>Kenneth Ugo</h2>
+        <body>"Insert Bio Here"<br></br><br></br><a href="https://www.linkedin.com/in/kenneth-ugo/">Connect with Kenneth on LinkedIn!</a>
+       </body>
+       </div>
+        </div>
+        </div>
+        </>
     );
 }
