b0bacafe4bb5c5fa8b0e8b8b1291f590db3c9f61
kenneth-alt <kenneth.ugo15@gmail.com>
2023-04-16 01:23:32 -0600

    rename userRoutes.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js"
-import { userRouter } from "./routes/userRoute.js"
+import { userRouter } from "./routes/userRoutes.js"
 
 
 dotenv.config()
