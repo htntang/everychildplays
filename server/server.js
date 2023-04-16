74aa95fd3c4612d5a10d708bf8fecd8ce503e038
kenneth-alt <kenneth.ugo15@gmail.com>
2023-04-15 20:17:46 -0600

    initialize server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import dotenv from "dotenv"
+
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 5005
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
