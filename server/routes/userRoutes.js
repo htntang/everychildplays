486b3d61484b1e361074dfbc22ab143ee33f2a16
kenneth-alt <kenneth.ugo15@gmail.com>
2023-04-16 15:56:03 -0600

    minor changes to userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,9 +3,16 @@ import { createUser, getUserById, updateUserById, deleteUserById } from '../cont
 
 const userRouter = express.Router();
 
+// Create a new user
 userRouter.post('/register', createUser);
+
+// Get a user by ID
 userRouter.get('/:id', getUserById);
+
+// Update a user by ID
 userRouter.patch('/:id', updateUserById);
+
+// Delete a user by ID
 userRouter.delete('/:id', deleteUserById);
 
 export default userRouter
