ed30da4206763358baeca7325ed0b74b410a294c
htntang <htntang@gmail.com>
2023-04-17 10:00:50 -0600

    made changes to contact and meet the team forms

--- client/src/Pages/Contact.js
+++ client/src/Pages/Contact.js
@@ -5,14 +5,14 @@ export default function Contact() {
         <h1>Contact Us!</h1>
         <form>
             <label>Name:</label>
-                <input type="text" name="name" />
+                <input type="text" className="form-control" /><br></br>
             <label>Email:</label>
-                <input type="text" name="email" />
+                <input type="text" className="form-control" /><br></br>
             <label>Subject:</label>
-                <input type="text" name="subject" />
-                <label>Questions, concerns, or comments?</label> 
+                <input type="text" className="form-control" /><br></br>
+                <label>Your message:</label> 
                 <br></br>   
-                <textarea className="form-control" id="message" required />
+                <textarea className="form-control" rows="8" id="message" required />
                 <br></br>
                 <br></br>
                 <input type="submit" value="Submit" />
--- client/src/Pages/Team.js
+++ client/src/Pages/Team.js
@@ -7,27 +7,56 @@ export default function Team() {
                 <div className="column">
                 <h2>Hikmah Adepoju</h2>
                 <img className="headshot" src="https://i.imgur.com/68rNYZA.jpg"></img>
-                <body className="bio">"Insert Bio Here"<br></br><br></br><a href="https://www.linkedin.com/in/hikmaha/">Connect with Hikmah on LinkedIn!</a>
+                <body className="bio">"Insert Bio Here"
+                <br></br>
+                <br></br>
+                <a href="https://www.linkedin.com/in/hikmaha/">Connect with Hikmah on LinkedIn!</a>
+                <br></br>
+                <br></br>
+                <a href="https://github.com/hadep275">Github</a>
                 </body></div>
 
                 <div className="column">
                 <h2>Oladimeji Falusi</h2>
                 <img className="headshot" src="https://i.imgur.com/nWlktNy.jpg"></img>
-                <body className="bio">"Insert Bio Here"<br></br><br></br><a href="https://www.linkedin.com/in/oladimeji-falusi-0251a225a/">Connect with Oladimeji on LinkedIn!</a>
+                <body className="bio">"Insert Bio Here"
+                <br></br>
+                <br></br>
+                <a href="https://www.linkedin.com/in/oladimeji-falusi-0251a225a/">Connect with Oladimeji on LinkedIn!</a>
+                <br></br>
+                <br></br>
+                <a href="https://github.com/ViktorOve">Github</a>
                 </body></div>
 
 
                 <div className="column">
                 <h2>Han Tang</h2>
                 <img className="headshot" src="https://i.imgur.com/d5AUyzC.jpg"></img>
-                <body className="bio">"Insert Bio Here"<br></br><br></br>
+                <body className="bio">
+                  "Insert Bio Here"
+                  <br></br>
+                  <br></br>
                 <a href="https://www.linkedin.com/in/han-tang-49ba799b/">Connect with Han on LinkedIn!</a>
+                <br></br>
+                <br></br>
+                <a href="https://github.com/htntang">Github</a>
                 </body>
                 </div>
+
                 <div className="column">
                 <h2>Kenneth Ugo</h2>
                 <img className="headshot" src="https://i.imgur.com/NEHq3O9.jpg"></img>
-                <body className="bio">"Insert Bio Here"<br></br><br></br><a href="https://www.linkedin.com/in/kenneth-ugo/">Connect with Kenneth on LinkedIn!</a>
+                <br></br>
+                <br></br>
+                <body className="bio">As a Cloud DevOps Engineer with over four years of experience, Kenneth has honed his ability to balance autonomy and teamwork, effectively utilizing cloud services and DevOps tools to manage complex systems and exceed expectations.
+                <br></br>
+                <br></br>Kenneth is committed to continuous learning and improvement, and approaching problem-solving with a self-driven mindset, while also collaborating effectively with any team.
+                <br></br>
+                <br></br>
+                <a href="https://www.linkedin.com/in/kenneth-ugo/">Connect with Kenneth on LinkedIn!</a>
+                <br></br>
+                <br></br>
+                <a href="https://github.com/kenneth-alt">Github</a>
             </body>
             </div>
                 </div>
