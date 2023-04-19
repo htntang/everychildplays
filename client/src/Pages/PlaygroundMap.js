adc5a8efcdeec3bf444e29ffaae17b23754d3693
htntang <htntang@gmail.com>
2023-04-18 23:34:19 -0600

    added json map points

--- client/src/Pages/PlaygroundMap.js
+++ client/src/Pages/PlaygroundMap.js
@@ -1,5 +1,21 @@
-export default function PlaygroundMap() {
-    return(
-        <h1>Playground Map</h1>
+import React, {useState} from "react";
+import ReactMapGL from "react-map-gl";
+
+export default function PlaygroundMap(){
+    const [viewport, setViewport] = useState({
+        latitude: 51.04539489746094,
+        longitude: -114.05809020996094,
+        width: '100vw',
+        height: '100vh',
+        zoom: 10,
+    });
+
+    return (
+    <div>
+        <ReactMapGL
+        {...viewport}
+        mapboxAccessToken="pk.eyJ1IjoiaHRudGFuZyIsImEiOiJjbGdsMjlhZDIwMDdnM2dxZ251eGY0MzBuIn0.RW4soJnBYI-87kzp-BUGzA" 
+        />
+    </div>
     );
 }
