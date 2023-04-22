0bdfad0f897c4fcc8a89f71e030e1c92cba4ec37
kenneth-alt <kenneth.ugo15@gmail.com>
2023-04-22 15:51:44 -0600

    map set up

--- client/src/Pages/PlaygroundMap.js
+++ client/src/Pages/PlaygroundMap.js
@@ -1,21 +1,24 @@
-import React, {useState} from "react";
-import ReactMapGL from "react-map-gl";
+import { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import PlaygroundMarker from '../assets/inclusive-playground-marker.svg';
 
-export default function PlaygroundMap(){
-    const [viewport, setViewport] = useState({
-        latitude: 51.04539489746094,
-        longitude: -114.05809020996094,
-        width: '100vw',
-        height: '100vh',
-        zoom: 10,
+function PlaygroundMap() {
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiaW5jbHVzaXZlcGxheXdvcmtzIiwiYSI6ImNsZ291NTlnZjBsYmQzZ2tjdGtqaWJsa3oifQ.XL2r23iiSDVExpHNlTxG0g';
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/inclusiveplayworks/clgsh6lgj000z01q1apkg1ft1',
+      center: [-114.0708, 51.0486],
+      zoom: 10,
     });
 
-    return (
-    <div>
-        <ReactMapGL
-        {...viewport}
-        mapboxAccessToken="pk.eyJ1IjoiaHRudGFuZyIsImEiOiJjbGdsMjlhZDIwMDdnM2dxZ251eGY0MzBuIn0.RW4soJnBYI-87kzp-BUGzA" 
-        />
-    </div>
-    );
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return <div id="map" style={{ height: '85vh', width: '75vw', display: 'flex', justifyContent: 'grow', alignItems: 'center' }} />;
 }
+
+export default PlaygroundMap;
