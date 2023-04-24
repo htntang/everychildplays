d6677144e95c1c55174bf08a6d306c1b17efd0be
htntang <htntang@gmail.com>
2023-04-24 14:09:30 -0600

    edited more tingz

--- client/src/Pages/Directory.js
+++ client/src/Pages/Directory.js
@@ -26,13 +26,13 @@ export default function Directory() {
           <Grid container spacing={2} justifyContent="center" alignItems="center">
             {playgrounds.map((playground) => (
               <Grid item xs={4} key={playground.name}>
-                <Card style={{ maxWidth: 335, backgroundColor: "#AEC595" }}>
+                <Card style={{ maxWidth: 335, backgroundColor: "#DD6E42" }}>
                   <CardMedia style={{ height: 140 }}
                     image={playground.pictures[0]}
                     title="inclusive-park"
                   />
                   <CardContent>
-                    <Typography gutterBottom variant="h5" component="div" color="#fffff0" fontFamily="Impact">
+                    <Typography gutterBottom variant="h5" component="div" color="#fffff0" fontFamily="sans-serif">
                       <b>{playground.name}</b>
                     </Typography>
                     <Typography variant="body2" color="#fffff0">
