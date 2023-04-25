3b7c761e0245b946aee2a73ade758a2b21e17292
kenneth-alt <kenneth.ugo15@gmail.com>
2023-04-24 21:00:51 -0600

    latest changes

--- client/src/Pages/Submit.js
+++ client/src/Pages/Submit.js
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import axios from 'axios';
-=======
-import React from "react";
-
-export default function Submit() {
-    return(
-        <form className="submission">
-            <h1>Submit a Playground!</h1>
->>>>>>> 4d777c1 (edited the navbar)
 
 const Submit = () => {
   const [parkData, setParkData] = useState({
