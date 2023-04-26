f4a981c5c8e079697fdf568d601ccdd02b0af77d
hadep275 <hadep275@gmail.com>
2023-04-25 21:00:10 -0600

    filter deleted

--- client/src/Pages/SearchPlaygrounds.js
+++ client/src/Pages/SearchPlaygrounds.js
@@ -1,61 +1,22 @@
 import { MuiTable2} from "../Components/MuiTableFromBackend.tsx"
 import {useState, useEffect} from "react";
-import axios from "axios";
-import MyDocument from "../Components/MuiTableFromBackend.tsx"
+import React from "react";
 
 export default function SearchPlaygrounds() {
     const [playgroundResults, setPlaygroundResults] =useState([]);
     const [searchTerm, setSearchTerm] = useState ("");
     const [filteredResults, setFilteredResults] = useState([]);
-    const [filters, setFilters] = useState({
-        accessibilityFeatures: false,
-        safetyFeatures: false,
-         // add additional filters here
-    });
+    
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //       try {
-    //         const response = await axios.get<MyDocument['']>(
-    //           "http://localhost:5005/api/playgrounds"
-    //         );
-    //         setPlaygroundResults(response.data);
-    //       } catch (error) {
-    //         console.error(error);
-    //       }
-    //     };
-    //     fetchData();
-    //   }, [searchTerm]);
+    const [fetchedData, setFetchedData] = React.useState(null);
 
+    function fetchData() {
+        fetch('http://localhost:5005/api/playgrounds')
+      .then(response => response.json())
+      .then(data => setFetchedData(data));
+    };
+    
 
-    useEffect(() => {
-    const filteredResults = playgroundResults.filter((result) => {
-        let include = true;
-            if (filters.accessibilityFeatures) {
-                include = include && result.accessibilityFeatures;
-            }
-            if (filters.safetyFeatures) {
-                include = include && result.securityCamera;
-            }
-            // add additional filters here
-            
-            return
-            include && result.name.toLowerCase().includes(searchTerm.toLowerCase());
-        });
-        setFilteredResults(filteredResults);
-      }, [playgroundResults, searchTerm, filters]);
-
-      const handleFilter = (filterTerm) => {
-        const filteredResults = playgroundResults.filter((result) => {
-          if (filterTerm === "accessibilityFeatures") {
-            return result.accessibilityFeatures === true;
-          } else if (filterTerm === "safetyFeatures") {
-            return result.safety_features === true;
-          }
-          return true;
-        });
-        setFilteredResults(filteredResults);
-      };
 
     
     return(
@@ -67,29 +28,6 @@ export default function SearchPlaygrounds() {
                     onChange={(event) => {
                         setSearchTerm(event.target.value);
                     }} /> 
-        {/* <button onClick={searchTerm}>Filter</button> */}
-        <div className="filters">
-                <label htmlFor="accessibilityFeaturesFilter">
-                    <input type="checkbox"
-                           id="accessibilityFeaturesFilter"
-                           checked={filters.accessibilityFeatures}
-                           onChange={(event) => {
-                               setFilters({...filters, accessibilityFeatures: event.target.checked})
-                           }} />
-                    Accessibility Features
-                </label>
-                <label htmlFor="safetyFeaturesFilter">
-                    <input type="checkbox"
-                           id="safetyFeaturesFilter"
-                           checked={filters.safetyFeatures}
-                           onChange={(event) => {
-                               setFilters({...filters, safetyFeatures: event.target.checked})
-                           }} />
-                    Safety Features
-                </label>
-                
-                {/* add additional filters here */}
-            </div>
 
             <ul className="list">
             <li className="listItem">
