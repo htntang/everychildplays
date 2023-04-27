c14d0c9069b0ef24c0ce455bf1606090489cc270
kenneth-alt <kenneth.ugo15@gmail.com>
2023-04-27 17:05:39 -0600

    updates to Directory.js

--- client/src/Pages/Directory.js
+++ client/src/Pages/Directory.js
@@ -1,23 +1,23 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Grid } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Rating from "react-rating-stars-component";
-import {
-  Grid,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Button,
-  Typography,
-} from "@mui/material";
-import ReviewPopup from "../Components/ReviewPopup";
-import ReadMorePopup from "../Components/ReadMorePopup";
 
 export default function Directory() {
   const [playgrounds, setPlaygrounds] = useState([]);
   const [selectedPlayground, setSelectedPlayground] = useState(null);
-  const [reviewPopupOpen, setReviewPopupOpen] = useState(false);
-  const [readMorePopupOpen, setReadMorePopupOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     axios.get("http://localhost:5005/api/playgrounds/").then((response) => {
@@ -25,50 +25,27 @@ export default function Directory() {
     });
   }, []);
 
-  const handleReviewSubmit = (formData) => {
-    const { rating, comment } = formData;
-    axios
-      .post(
-        `http://localhost:5005/api/playgrounds/${selectedPlayground._id}/reviews/create`,
-        {
-          rating,
-          comment,
-        }
-      )
-      .then(() => {
-        setReviewPopupOpen(false);
-        setSelectedPlayground(null);
-      })
-      .catch((err) => console.log(err));
+  const handleOpen = (playground) => {
+    setSelectedPlayground(playground);
+    setOpen(true);
   };
 
-  const handleReadMoreClick = (playground) => {
-    setSelectedPlayground(playground);
-    setReadMorePopupOpen(true);
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
     <>
       <h1>Directory & Reviews</h1>
 
-      <div className="reviews" style={{ padding: 30 }}>
+      <div className="directory-container">
         <Grid container spacing={2} justifyContent="center" alignItems="center">
           {playgrounds.map((playground) => (
             <Grid item xs={4} key={playground.name}>
               <Card style={{ maxWidth: 335, backgroundColor: "#DD6E42" }}>
-                <CardMedia
-                  style={{ height: 140 }}
-                  image={playground.pictures[0]}
-                  title="inclusive-park"
-                />
+                <CardMedia style={{ height: 140 }} image={playground.pictures[0]} title="inclusive-park" />
                 <CardContent>
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="div"
-                    color="#fffff0"
-                    fontFamily="sans-serif"
-                  >
+                  <Typography gutterBottom variant="h5" component="div" color="#fffff0" fontFamily="sans-serif">
                     <b>{playground.name}</b>
                   </Typography>
                   <Typography variant="body2" color="#fffff0">
@@ -79,19 +56,7 @@ export default function Directory() {
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button
-                    size="small"
-                    onClick={() => {
-                      setSelectedPlayground(playground);
-                      setReviewPopupOpen(true);
-                    }}
-                  >
-                    Add Review
-                  </Button>
-                  <Button
-                    size="small"
-                    onClick={() => handleReadMoreClick(playground)}
-                  >
+                  <Button size="small" onClick={() => handleOpen(playground)}>
                     Read More
                   </Button>
                 </CardActions>
@@ -100,65 +65,27 @@ export default function Directory() {
           ))}
         </Grid>
       </div>
-      {reviewPopupOpen && (
-        <ReviewPopup
-          open={reviewPopupOpen}
-          handleClose={() => setReviewPopupOpen(false)}
-          handleReviewSubmit={handleReviewSubmit}
-          playground={selectedPlayground}
-        />
-      )}
-      {readMorePopupOpen && (
-        <ReadMorePopup
-          open={readMorePopupOpen}
-          handleClose={() => setReadMorePopupOpen(false)}
-          playground={selectedPlayground}
-        />
-      )}
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{selectedPlayground && selectedPlayground.name}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <b>Location:</b> {selectedPlayground && selectedPlayground.location}
+            <br />
+            <br />
+            <b>Description:</b> {selectedPlayground && selectedPlayground.description}
+            <br />
+            <br />
+            <b>Accessibility Features:</b> {selectedPlayground && selectedPlayground.accessibilityFeatures}
+            <br />
+            <br />
+            <b>Safety Features:</b> {selectedPlayground && selectedPlayground.safetyFeatures}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
-
-
-//       axios.get("http://localhost:5005/api/playgrounds/").then((response) => {
-//         setPlaygrounds(response.data);
-//       });
-//     }, []);
-  
-//     return (
-//       <>
-//         <h1>Directory & Reviews</h1>
-  
-//         <div className="reviews" style={{ padding: 30 }}>
-//           <Grid container spacing={2} justifyContent="center" alignItems="center">
-//             {playgrounds.map((playground) => (
-//               <Grid item xs={4} key={playground.name}>
-//                 <Card style={{ maxWidth: 335, backgroundColor: "#DD6E42" }}>
-//                   <CardMedia style={{ height: 140 }}
-//                     image={playground.pictures[0]}
-//                     title="inclusive-park"
-//                   />
-//                   <CardContent>
-//                     <Typography gutterBottom variant="h5" component="div" color="#fffff0" fontFamily="sans-serif">
-//                       <b>{playground.name}</b>
-//                     </Typography>
-//                     <Typography variant="body2" color="#fffff0">
-//                       <i>{playground.location}</i>
-//                       <br />
-//                       <br />
-//                       {playground.description}
-//                     </Typography>
-//                   </CardContent>
-//                   <CardActions>
-//                     <Button size="small">Add Review</Button>
-//                     <Button size="small">Read More</Button>
-//                   </CardActions>
-//                 </Card>
-//               </Grid>
-//             ))}
-//           </Grid>
-//         </div>
-//       </>
-//     );
-//   }
-
