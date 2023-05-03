a4d8fe652d3280ec9e9604aaeb8d864cf471c16a
kenneth-alt <kenneth.ugo15@gmail.com>
2023-05-03 00:05:36 -0600

    update map

--- client/src/Pages/PlaygroundMap.jsx
+++ client/src/Pages/PlaygroundMap.jsx
@@ -1,68 +1,94 @@
-import "mapbox-gl/dist/mapbox-gl.css";
-import { useEffect, useState } from "react";
-import mapboxgl from "mapbox-gl";
+import React, { useRef, useEffect, useState } from "react";
 import axios from "axios";
-import React from 'react'
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
+import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css";
 
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
-function PlaygroundMap() {
+const PlaygroundMap = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-114.0571411);
+  const [lat, setLat] = useState(51.0453775);
+  const [zoom, setZoom] = useState(10);
+
   const [markers, setMarkers] = useState([]);
 
   useEffect(() => {
-    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
-    const map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/ollysamm/clgmqv6wz001f01pl6kx5gu1y",
-      center: [-114.0708, 51.0486],
-      zoom: 10
-    });
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/outdoors-v12",
+      center: [lng, lat],
+      zoom: zoom,
+    })
+    .addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+        showUserHeading: true,
+        showUserLocation: true,
+      })
+    )
 
-    axios
-      .get("http://localhost:5005/api/playgrounds")
-      .then(response => {
-        const playgroundData = response.data;
+    // .addControl(new mapboxgl.NavigationControl({ showSearch: false }))
+    // .addControl(
+    //   new MapboxGeocoder({
+    //     accessToken: mapboxgl.accessToken,
+    //     mapboxgl: mapboxgl,
+    //   }),
+    //   "top-left"
+    // )
+    
+    .addControl(
+      new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+        unit: "metric",
+        profile: "mapbox/walking, mapbox/driving",
+      }),
+      "top-left"
+    );
 
-        const newMarkers = playgroundData.map(playground => {
+    async function fetchPlaygrounds() {
+      try {
+        const response = await axios.get(
+          "http://localhost:5005/api/playgrounds"
+        );
+        const playgrounds = response.data;
+
+        playgrounds.forEach((playground) => {
           const markerElement = document.createElement("div");
-					markerElement.classList.add("playground-marker");
+          markerElement.classList.add("playground-marker");
 
-
-          const newMarker = new mapboxgl.Marker({
+          new mapboxgl.Marker({
             element: markerElement,
             color: "#3FB1CE",
-            draggable: false
+            draggable: false,
+            anchor: "top-right",
           })
             .setLngLat([playground.longitude, playground.latitude])
-            .setPopup(new mapboxgl.Popup({ offset: 15 }).setHTML(`<h3 style='color: black;'>${playground.name}</h3><p style='color: black;'>${playground.location}</p><p style='color: black;'>${playground.quadrant}</p>`))
-            .addTo(map);
-
-          return newMarker;
+            .setPopup(
+              new mapboxgl.Popup({ offset: 15 }).setHTML(
+                `<div style="color: black; background-color: rgba(0, 0, 0, 0);"><img src="${playground.pictures}" alt="${playground.name}" /><h3>${playground.name}</h3><p>${playground.location}</p></div>`
+              )
+            )
+            .addTo(map.current);
         });
-
-        setMarkers(newMarkers);
-
-        return () => {
-          newMarkers.forEach(marker => marker.remove());
-          map.remove();
-        };
-      })
-      .catch(error => {
+      } catch (error) {
         console.error(error);
-      });
+      }
+    }
+
+    fetchPlaygrounds();
   }, []);
 
-  return (
-    <div
-      id="map"
-      style={{
-        height: "85vh",
-        width: "75vw",
-        display: "flex",
-        justifyContent: "grow",
-        alignItems: "center"
-      }}
-    />
-  );
-}
+  return <div ref={mapContainer} style={{ height: "85vh" }} />;
+};
 
 export default PlaygroundMap;
